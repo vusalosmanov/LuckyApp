3dca4b015ca97ba840dba632165e4bd57e7c2801
Vusal Osmanov <115807599+vusalosmanov@users.noreply.github.com>
2023-10-19 21:26:01 +0400

    add ProductCart alert and designer

--- src/pages/ShopProduct.jsx
+++ src/pages/ShopProduct.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from "react-redux";
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-// import '../assets/styles/pages/ShopProduct.scss'
+import TiDelete from 'react-icons/ti'
 import { AddCart, DecreaseCart, RemoveFromCart } from '../redux/cartSystem';
 import { useDispatch } from 'react-redux';
 const ShopProduct = () => {
@@ -30,86 +30,89 @@ const ShopProduct = () => {
         dispatch(RemoveFromCart({ id: productId }));
     };
 
+    const [alert, setAlert] = useState(false);
+    const [alertText, setAlertText] = useState('');
 
-    const [alert, setAlert] = useState(false)
-    const [alertText, setAlertText] = useState('')
-
-    let minOrder = 5
+    let minOrder = 5;
 
     useEffect(() => {
         if (sumPrice < minOrder && sumPrice !== 0) {
             setAlert(false);
             setAlertText(`Sifarişi təstiqləmək üçün minimum ${minOrder} Azn olmalıdır.`);
+
         } else {
             setAlert(false);
         }
-    }, [sumPrice, minOrder])
+    }, [sumPrice, minOrder]);
 
     const orderHandleClick = () => {
         if (sumPrice === 0) {
             setAlert(true);
             setAlertText('Sifarişi təsdiqləmək üçün səbətinizdə ən az bir məhsul olmalıdır.');
-
+        } else if (sumPrice < minOrder) {
+            setAlert(true);
+            setAlertText(`Sifarişi təstiqləmək üçün minimum ${minOrder} Azn olmalıdır.`);
         } else {
             setAlert(true);
+            setAlertText('tesdiqlendi');
+
         }
     };
+
     return (
         <>
             {cart.length > 0 ? (
-
-                <div className='lg:w-[1170px] w-full  my-0 mx-auto  pr-[15px] pl-[15px] lg:block md:block hidden  mb-[20px]'>
-                    <div className={`${alert ? 'w-[700px] h-[50px] bg-orange-500 flex items-center justify-center  text-[white] text-[18px] mt-[20px] mb-[20px]' : 'hidden'}`} >
-                        <p>{alertText}</p>
-                    </div>
-                    <table id='customers' className='lg:w-[1170px]'>
-                        <thead className="text-center bg-[#f7f7f7] text-[16px]">
-                            <tr className="border-b-[1px] text-[#4d4d4d]">
-                                <th className="border-[1px] p-[8px]">Mehsulun adi</th>
-                                <th className="border-[1px] p-[8px]">Qiymet</th>
-                                <th className="border-[1px] p-[8px] font-[400]">Say</th>
-                                <th className="p-[8px] font-[200]">Cemi</th>
-                            </tr>
-                        </thead>
-                        {cart.map(product => (
-                            <>
-                                <table id="customers">
-                                    <tr className="border-[1px] mt-[20px] h-[168px]">
-                                        <td className="border-[1px] w-[168px] p-[8px]">
-                                            <img src={product.imageurl} alt="" />
+                <div className='w-[1320px] my-0 mx-auto pr-[10px] pl-[10px] flex justify-center items-center'>
+                    <div className='mb-[40px] w-[100%] overflow-auto'>
+                        <div className='w-[100%] min-w-[1200px] flex items-center justify-center'>
+                            <div className={`${alert ? 'min-w-[1200px]  h-[50px] bg-orange-500 flex items-center justify-center rounded-lg  text-[white] text-[18px] mt-[20px] mb-[20px]' : 'hidden'}`} >
+                                <p>{alertText}</p>
+                            </div>
+                        </div>
+                        <table className='mb-[50px] min-w-[1200px] w-[100%] border-collapse  caption-bottom mt-[50px]'>
+                            <thead>
+                                <tr className='border-b-[2px]'>
+                                    <th className='py-[15px] px-[0px] text-center' ></th>
+                                    <th className='py-[15px] px-[0px] text-center' >Məhsulun adı</th>
+                                    <th className='py-[15px] px-[0px] text-center' >Qiymət</th>
+                                    <th className='py-[15px] px-[0px] text-center' >Sayı</th>
+                                    <th className='py-[15px] px-[0px] text-center' >Cəmi</th>
+                                    <th className='py-[15px] px-[0px] text-center' ></th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                {cart.map(product => (
+                                    <tr className='border-b-[2px]'>
+                                        <td className=" w-[100px]  h-[100px] py-[10px]">
+                                            <img src={product.imageurl} alt="" className='w-[100%] h-[100%] object-contain' />
                                         </td>
-                                        <td className="border-[1px] p-[8px] text-[#3a3a3a] text-[14px] capitalize w-[260px] font-[400]">
+                                        <td className='py-[10px] px-[0px] text-[18px] text-center'>
                                             <p>{product.productName}</p>
                                         </td>
-                                        <td className="border-[1px] p-[8px] text-[#3a3a3a] text-[14px] capitalize w-[260px] font-[400]">
-                                            <p>{product.price}</p>
+                                        <td className="py-[10px] text-[20px] text-center min-w-[150px] capitalize font-[400]">
+                                            <p>{product.price.toFixed(2)} AZN</p>
                                         </td>
-                                        <td className=" border-[1px] justify-center items-center p-[8px] w-[150px]">
+                                        <td className=" justify-center items-center py-[10px] w-[150px]">
                                             <div className="flex gap-3 justify-around mb-4">
-                                                <div className="pt-[2px] pb-[2px] pl-[6px] pr-[6px] rounded-[5px] text-[white] bg-[#f15803] flex justify-center items-center">
-                                                    <button onClick={() => handleDecreaseQuantity(product.id)}>-</button>
-                                                </div>
-                                                <div className="pt-[6px] pb-[6px] pl-[12px] pr-[12px] rounded-[5px] text-[white] bg-[#f15803] ">
-                                                    <span>{product.quantity}</span>
-                                                </div>
-                                                <div className="pt-[2px] pb-[2px] pl-[8px] pr-[8px] rounded-[5px] text-[white] bg-[#f15803] flex justify-center items-center ">
-                                                    <button onClick={() => handleIncreaseQuantity(product.id)}>+</button>
-                                                </div>
+                                                <button onClick={() => handleDecreaseQuantity(product.id)} className='w-[40px] h-[40px] text-[white] bg-orange-500 rounded-[5px]'>-</button>
+                                                <span className='min-w-[50px] my-0 mx-[10px] text-center inline-block text-[18px] bg-[white] border-[1px] py-[5px] px-[10px] '>{product.quantity}</span>
+                                                <button onClick={() => handleIncreaseQuantity(product.id)} className='w-[40px] h-[40px] text-[white] bg-orange-500 rounded-[5px]'>+</button>
                                             </div>
                                         </td>
-                                        <td className="border-[1px] p-[8px] text-[#f15803] font-[200] w-[150px] ">
-                                            {(product.price * product.quantity).toFixed(2)}
+                                        <td className="py-[10px] text-[20px] text-center min-w-[150px]">
+                                            <span>
+                                                {(product.price * product.quantity).toFixed(2)} AZN
+                                            </span>
                                         </td>
                                         <td>
-                                            <button onClick={() => handleRemoveFromCart(product.id)}>Sil</button>
+                                            <button onClick={() => handleRemoveFromCart(product.id)} className='bg-[#c82333] text-[white] w-[40px] h-[40px] rounded-[5px] text-[22px]'><i className='fa-solid fa-xmark'></i></button>
                                         </td>
                                     </tr>
-                                </table>
-                            </>
-
-                        ))}
-                    </table >
-                </div >
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             ) : null}
             <div className='w-[1320px] mx-auto pr-[10px] pl-[10px] flex justify-end items-center mb-[20px]'>
                 {cart.length > 0 ? (
